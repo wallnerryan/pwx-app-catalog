959d97b8f8ab3312dae50284465e9d18b87fd253
Ryan Wallner <wallnerryan@users.noreply.github.com>
2021-02-04 10:15:19 -0500

    Update receive.py

--- apps/Messaging/RabbitMQ/receive.py
+++ apps/Messaging/RabbitMQ/receive.py
@@ -4,10 +4,9 @@ import sys
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
-    sys.exit(0) 
 
 credentials = pika.PlainCredentials('admin', 'secretpassword')
-connection = pika.BlockingConnection(pika.ConnectionParameters('70.0.86.158',30785, '/',credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters('10.233.47.44',5672, '/',credentials))
 channel = connection.channel()
 channel.queue_declare(queue='hello', durable=True)
 channel.basic_consume(queue='hello',
